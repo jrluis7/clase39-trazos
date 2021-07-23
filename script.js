b147408eca2048cce403b6223ff53ad200c988a1
jrluis7 <jrluis7@gmail.com>
2021-07-23 09:43:39 +0200

    Commit inicial.  Creación de los archivos y link de los recursos

---
+++ script.js
@@ -0,0 +1,19 @@
+// Control de Versiones - GIT , Mercurial
+
+// Volver a versiones antiguas de desarrollo
+
+// Volver a versiones estables de un desarrollo
+
+// Trabajar en grupo sobre un proyecto
+
+// Trabajar en el mismo proyecto desde cualquier ordenador
+
+// Git puede usarse por consola
+// Hay clientes de GIT con interfaz gráfica -> SOURCETREE
+
+
+// - Tracked: Git está pendiente de los cambios del archivo
+// - Staged:  Son archivos que están preparados para un nuevo Commit
+// - Commit: Es una foto, o un punto de guardado con los cambios propuestos
+
+console.log( 'Cargando el script' )
