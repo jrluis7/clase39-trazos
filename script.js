07f75a6de7898440b52ae702db285ae00e3c02fe
jrluis7 <jrluis7@gmail.com>
2021-07-23 10:08:24 +0200

    incluir un nuevo h3

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 // Git puede usarse por consola
 // Hay clientes de GIT con interfaz gráfica -> SOURCETREE
 
+// SourceTree -> Es una interfaz gráfica que te ayuda a ver tu repositorio LOCAL ( tu ordenador ) de una fomra amable ( No se guarda en internet en nngún sitio, por el momento )
 
 // - Tracked: Git está pendiente de los cambios del archivo
 // - Staged:  Son archivos que están preparados para un nuevo Commit
